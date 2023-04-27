e1a30483b27410817ea36a0aed30103c18bd2a8a
Do HUYNH <huynhdoo@gmail.com>
2023-04-27 17:59:03 +0200

    Add Vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,19 @@
 import image from "@astrojs/image";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
+import vercelEdge from "@astrojs/vercel/edge";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://inter-respons-ai.vercel.app",
   output: "server",
-  integrations: [
-    tailwind(),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
-  ],
+  adapter: vercelEdge(),
+  integrations: [tailwind(), image({
+    serviceEntryPoint: "@astrojs/image/sharp"
+  })],
   vite: {
     ssr: {
-      external: ["svgo"],
-    },
-  },
+      external: ["svgo"]
+    }
+  }
 });
